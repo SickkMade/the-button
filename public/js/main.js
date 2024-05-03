0e5b481eca8b6c6f1af6c1c5f95a83378f7784bb
Jack Meyer <jaakmyr@gmail.com>
2024-05-02 19:42:56 -0700

    changeing size of button on click, trying to add shake but is bug

--- public/js/main.js
+++ public/js/main.js
@@ -14,16 +14,23 @@ button.addEventListener('mousedown', () => {
         element.classList.add('buttonClickAnimation')
     })
     boxResize.classList.add('boxResizeAnimation')
+    button.classList.add('shake');
 });
 button.addEventListener('mouseup', () => {
     needAnimation.forEach( element => {
         element.classList.remove('buttonClickAnimation')
     })
     boxResize.classList.remove('boxResizeAnimation')
+    button.classList.remove('shake');
     onButtonClick()
 });
 
 async function onButtonClick(){
+    //we are goin to maek button click change the size of button
+    let scale = 0; 
+    
+
+
     try{
         const response = await fetch('/updateButtonCount', {
             method:'put'
@@ -31,6 +38,9 @@ async function onButtonClick(){
         const data = await response.json()
         buttonCount.textContent = data.score
         maxCount.textContent = "Max Score: "+ data.maxScore
+        
+        scale = 0.85 + (0.01 * data.score)
+        button.style.transform = `translate(-50%, -50%) scale(${scale})`;
 
         if(data.didFail) {
             explosionAudio.src = "sounds/explosion" + Math.floor(Math.random()* 3 +1) + ".wav"
